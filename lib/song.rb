e9e34cb161f0cbcada49bc14d07cf1d5d87f7dc8
Alan Tang <giabao_tang@yahoo.com>
2020-10-14 23:12:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,10 +30,11 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #def to_param
+   # name.downcase.gsub(' ', '-')
+  #end
   
   extend Memorable
+  include Paramable
 end
 
