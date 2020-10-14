8f480139d1dae4c874eab3b3bfd004eacc79604a
Alan Tang <giabao_tang@yahoo.com>
2020-10-14 23:34:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,9 +6,9 @@ class Song
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  #def initialize
+   # @@songs << self
+  #end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
@@ -34,7 +34,8 @@ class Song
    # name.downcase.gsub(' ', '-')
   #end
   
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   include Paramable
 end
 
